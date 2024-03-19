4baa442e18d41dff62f74c80f395685b17c67a5f
Louistam888 <louistam888@gmail.com>
2024-03-18 21:11:07 -0400

    finished second module

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,7 +1,26 @@
 import { Component } from '@angular/core';
+import { CoursesService } from './courses.service';
 
 @Component({
   selector: 'courses',
-  template: '<h2>Courses</h2>',
+  template: `
+    <h2>{{ title }}</h2>
+    <ul>
+      <li *ngFor="let course of courses">
+        {{ course }}
+      </li>
+    </ul>
+  `,
 })
-export class CoursesComponent {}
+
+//service
+export class CoursesComponent {
+  title = 'List of Courses';
+  courses;
+  //dependency injuection is injecting dependency of the class into the constructor
+
+  constructor(service: CoursesService) {
+    this.courses = service.getCourses();
+  }
+  //logic for calling a http service
+}
