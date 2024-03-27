472903dc8db8f354194a0002ad2aa5546bd88a5b
Louistam888 <louistam888@gmail.com>
2024-03-26 20:44:40 -0400

    lesson 9 displaying data

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,24 +3,13 @@ import { CoursesService } from './courses.service';
 
 @Component({
   selector: 'courses',
-  template: `
-    <h2>{{ title }}</h2>
-    <ul>
-      <li *ngFor="let course of courses">
-        {{ course }}
-      </li>
-    </ul>
-  `,
+  template: ` <input (keyup.enter)="onKeyUp()" /> `,
 })
 
 //service
 export class CoursesComponent {
-  title = 'List of Courses';
-  courses;
-  //dependency injuection is injecting dependency of the class into the constructor
-
-  constructor(service: CoursesService) {
-    this.courses = service.getCourses();
+  onKeyUp() {
+    console.log('Enter key pressed');
   }
-  //logic for calling a http service
 }
+//9 template variables
