8f8a823d4224350da91b4c58e270f4f1ada17448
Louistam888 <louistam888@gmail.com>
2024-04-01 01:17:36 -0400

    commit 4-1-24

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,10 +9,17 @@ import { CourseComponent } from './course/course.component';
 import { CoursesService } from './courses.service';
 import { AuthorsComponent } from './authors/authors.component';
 import { AuthorsService } from './authors.service';
+import { SummaryPipe } from './summary.pipe';
 
 @NgModule({
-  declarations: [AppComponent, CoursesComponent, CourseComponent, AuthorsComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    CoursesComponent,
+    CourseComponent,
+    AuthorsComponent,
+    SummaryPipe
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [CoursesService, AuthorsService],
   bootstrap: [AppComponent],
 })
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,15 +1,35 @@
 import { Component } from '@angular/core';
-import { CoursesService } from './courses.service';
 
 @Component({
   selector: 'courses',
-  template: ` <input (keyup.enter)="onKeyUp()" /> `,
+  template: `
+    <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" /> <br />
+
+    {{ course.title | uppercase }} <br />
+    {{ course.students | number }}<br />
+    {{ course.rating | number : '1.2-2' }}<br />
+    {{ course.price | currency : 'AUD' : true : '3.2-2' }}<br />
+    {{ course.releaseDate | date : 'shortDate' }}<br />
+    {{ text | summary : 10 }}
+  `,
 })
 
 //service
 export class CoursesComponent {
+  course = {
+    title: 'Angular COurse',
+    rating: 2334,
+    students: 43535,
+    price: 2329.93,
+    releaseDate: new Date(2016, 3, 1),
+  };
+
+  text = `This is asampel test summarisze tihs please adlkfjdlkajf  ljadflkjalkfjdkljfao;sdfj daljf;ldjfdae`;
+
+  email = 'me@expample.com';
+
   onKeyUp() {
-    console.log('Enter key pressed');
+    console.log(this.email);
   }
 }
 //9 template variables
